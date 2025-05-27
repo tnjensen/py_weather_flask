8670f7212599963453ec36bed7aa3f6a7e23c8cc
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2025-05-27 19:17:25 +0200

    Reverted changes

--- main.py
+++ main.py
@@ -4,23 +4,25 @@ from waitress import serve
 
 app = Flask(__name__) #Creates a flask app
 
-""" @app.route("/") """ #Defines the route for the home page
-@app.route("/index.html")
+@app.route("/") #Defines the route for the home page
+@app.route("/index")
 def index():
     return render_template("index.html")
 
-@app.route("/weather.html")
-def weather():
-    city = request.args.get("city") #Gets the city from the query string and removes leading and trailing spaces
+@app.route("/weather")
+def get_weather():
+    city = request.args.get("city")
+    if not bool(city.strip()):
+        city = "Trondheim"
     weather_data = get_current_weather(city)
     #City not found by API
     if weather_data["cod"] != 200:
-        """ return render_template(
-            "./not_found.html", 
-        ) """
-        print(f"City not found: {city}")
+        return render_template(
+            "not_found.html", 
+            
+        )
     return render_template(
-        "./weather.html", 
+        "weather.html", 
         title=weather_data["name"],
         status=weather_data["weather"][0]["description"].capitalize(),
         temp=f"{weather_data["main"]["temp"]:.1f}",
@@ -30,4 +32,3 @@ def weather():
 if __name__ == "__main__":
     #app.run(host="0.0.0.0", port=8000) #Runs the app on port 8000 in Dev mode
     serve(app, host="0.0.0.0", port=8000)
-    """ app.run(debug=True) """
